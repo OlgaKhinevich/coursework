6296dc900787ce56970517a0e2fd1daaed97acba
OlgaKhinevich <olga-khinevich@yandex.ru>
2019-05-21 13:51:01 +0300

    last changes

--- CSqMatrix.h
+++ CSqMatrix.h
@@ -9,75 +9,103 @@ private:
 	int size;
 	std::vector <std::vector<T>> matrix;
 	std::vector <T> column;
-	std::vector <T> sum_rows;
+	
 public:
 	// конструктор 
-	inline CSqMatrix() {}
+	CSqMatrix() {}
 
-	// конструктор копирования
-	inline CSqMatrix(const CSqMatrix& first_matrix) :size(first_matrix.size), matrix(first_matrix.matrix), column(first_matrix.column)
+	// деструктор
+	~CSqMatrix() {	
+		size = 0;
+		matrix.clear();
+		column.clear();
+	}
+
+	// конструктор копирования объекта
+	CSqMatrix(const CSqMatrix& first_matrix) :size(first_matrix.size), matrix(first_matrix.matrix), column(first_matrix.column)
 	{
-		if (matrix.size() <= 0)
-			throw std::exception("Невозможно создать конструктор");
-		std::cout << "Конструктор копирования сработал!" << std::endl; // строка для проверки работы конструктора
+		std::cout << "Конструктор копирования сработал!" << std::endl;
 	}
 
 	//оператор присваивания копированием
-	inline CSqMatrix& operator=(const CSqMatrix& first_matrix)
+	CSqMatrix& operator = (const CSqMatrix& first_matrix)
 	{
-		// проверка на самоприсваивание
 		if (&first_matrix == this)
 			return *this;
-		// Удаляем всё, что может хранить указатель до этого момента
-		delete matrix;
-		delete column;
-
-		// Копируем передаваемый объект
-		matrix = new std::vector<std::vector<T>>;
-		*matrix = *first_matrix.matrix;
-		column = new std::vector<T>;
-		*column = *first_matrix.column;
-
+		matrix.clear();
+		column.clear();
+		size = first_matrix.size;
+		matrix = first_matrix.matrix;
+		column = first_matrix.column;
+		std::cout << "Оператор присваивания копированием сработал!" << std::endl;
 		return *this;
 	}
 
 	//оператор присваивания перемещением
-	inline CSqMatrix& operator=(CSqMatrix&& first_matrix)
+	CSqMatrix& operator=(CSqMatrix&& first_matrix)
 	{
-		size = move(first_matrix.size);
-		matrix = move(first_matrix.matrix);
-		column = move(first_matrix.column);
+		size = std::move(first_matrix.size);
+		matrix = std::move(first_matrix.matrix);
+		column = std::move(first_matrix.column);
 		first_matrix.size = 0;
-		first_matrix.column = nullptr;
-		first_matrix.matrix = nullptr;
-
+		std::cout << "Оператор присваивания перемещением сработал!" << std::endl;
 		return *this;
+		
 	}
 
 	//конструктор перемещения
-	inline CSqMatrix(CSqMatrix&& first_matrix) :size(first_matrix.size), matrix(first_matrix.matrix), column(first_matrix.column)
+	CSqMatrix(CSqMatrix&& first_matrix) :size(std::move(first_matrix.size)), matrix(std::move(first_matrix.matrix)), column(std::move(first_matrix.column))
 	{
 		first_matrix.size = 0;
-		first_matrix.matrix = nullptr;
-		first_matrix.column = nullptr;
 		std::cout << "Конструктор перемещения сработал!" << std::endl;
 	}
 
-
 	// ввод размера матрицы
-	inline int input_matrix_size()
+	void input_matrix_size (std::istream& InputStream)
+	{
+		InputStream >> size;
+		if (InputStream.fail() || size <= 0)
+		{
+			throw std::underflow_error("Размер матрицы имеет недопустимое значение!");
+		}
+	}
+
+	// прототип метода ввода матрицы
+	void input_matrix(std::istream& InputStream, std::ostream& OutputStream);
+	
+	// прототип метода вывода матрицы
+	void output_matrix(std::ostream& OutputStream);
+
+	// удаление матрицы
+	void delete_matrix()
 	{
 		size = 0;
-		std::cin >> size;
-		if (std::cin.fail() || size <= 0)
-		{
-			throw std::length_error("");
-		}
+		matrix.clear();
+		column.clear();
+	}
+
+	// получение текущего размера матрицы
+	int get_size() 
+	{
 		return size;
 	}
 
-	// ввод матрицы
-	inline void input_matrix(std::istream& InputStream, std::ostream& OutputStream)
+	// прототипа метода получения максимального и минимального элементов
+	auto min_max();
+
+	// прототип метода получения суммы элементов над главной диагональю 
+	auto sum_above_diagonal();
+
+	// прототип метода получения суммы элементов строки
+	auto row_amount(int row_number);
+
+	// прототип сортировки
+	void sort_matrix();
+};
+
+// реализация метода ввода матрицы 
+template<class T>
+void CSqMatrix<T>::input_matrix(std::istream & InputStream, std::ostream & OutputStream)
 	{
 		T col;
 		for (int i = 0; i < size; i++)
@@ -89,159 +117,133 @@ public:
 				InputStream >> col;
 				column.push_back(col);
 				if (InputStream.fail())
-					throw std::exception("Элемент не введен!");
+					throw std::invalid_argument("Введенное значение не является числом!");
 			}
 			matrix.push_back(column);
 		}
 	}
 
-	// вывод матрицы
-	inline void output_matrix(std::ostream& OutputStream)
-	{
-		if (!matrix.empty())
-		{
-			for (int i = 0; i < size; i++)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					OutputStream << matrix[i][j] << '\t';
-				}
-				OutputStream << std::endl;
-			}
-		}
-
-	}
-
-	// удаление матрицы
-	inline void delete_matrix()
-	{
-		size = 0;
-		matrix.clear();
-		column.clear();
-	}
-
-	// получение текущего размера матрицы
-	inline void get_size(std::ostream& OutputStream)
-	{
-		int i = 0,
-			j = 0;
-		if (matrix.size() <= 0)
-		{
-			throw std::exception("Размер матрицы неверный!");
-		}
-
-		OutputStream << matrix[i].size() << "x" << matrix[j].size() << std::endl;
-	}
-
-	// максимальный и минимальный элементы
-	inline auto min_max()
-	{
-		T min = matrix[0][0];
-		T max = min;
-		std::vector <T> min_elements;
-		std::vector <T> max_elements;
-		std::vector <T> results;
-		
-		T min_result;
-		T max_result;
-
-		if (!matrix.empty())
-		{
-			for (int i = 0; i < size; i++)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					if (matrix[i][j] < min)
-						min = matrix[i][j];
-					else if (matrix[i][j] > max)
-						max = matrix[i][j];
-					else continue;
-				}
-				min_elements.push_back(min);
-				max_elements.push_back(max);	
-			}
-			min_result = *min_element(min_elements.begin(), min_elements.end());
-			max_result = *max_element(max_elements.begin(), max_elements.end());
-			results.push_back(1);
-			results.push_back(min_result);
-			results.push_back(max_result);
-			return results;
-		}
-		else
-		{
-			results.push_back(0);
-			results.push_back(0);
-			results.push_back(0);
-			return results;
-		}
-	}
-
-
-	// сумма элементов над главной диагональю 
-	inline auto sum_above_diagonal()
-	{
-		T sum = 0;
-		matrix.resize(size, std::vector<T>(size));
-		if (!matrix.empty())
-		{
-			for (int i = 0; i < size; i++)
-			{
-				for (int j = 0; j < size; j++)
-				{
-					if (j > i)
-					{
-						sum += matrix[i][j];
-					}
-					else continue;
-				}
-			}
-			return std::make_pair(true, sum);
-		}
-		else {
-			return std::make_pair(false, sum);
-		}
-	}
-
-	// сумма элементов строки
-	inline auto row_amount()
+// реализация метода вывода матрицы
+template<class T>
+void CSqMatrix<T>::output_matrix(std::ostream & OutputStream)
+{
+	if (!matrix.empty())
 	{
 		for (int i = 0; i < size; i++)
 		{
-			T row_sum = 0;
 			for (int j = 0; j < size; j++)
 			{
-				row_sum += matrix[i][j];
+				OutputStream << matrix[i][j] << '\t';
 			}
-			sum_rows.push_back(row_sum);
+			OutputStream << std::endl;
 		}
-		return sum_rows;
 	}
 
-	// сортировка
-	inline void sort_matrix()
+}
+
+// реализация метода получения максимального и минимального элементов
+template<class T>
+auto CSqMatrix<T>::min_max()
+{
+	T min = matrix[0][0];
+	T max = min;
+	std::vector <T> min_elements;
+	std::vector <T> max_elements;
+	std::vector <T> results;
+
+	T min_result;
+	T max_result;
+
+	if (!matrix.empty())
 	{
-		for (int i = 0; i < size; i++) {
-			// Вытаскиваем строку
-			auto value = matrix[i];
-			// Сумма первой строки
-			auto current_sum = sum_rows[i];
-			// Перемещаемся по cтрокам, которые перед вытащенной строкой
-			int k = i - 1;
-			for (; k >= 0; k--) {
-				auto prev_sum = sum_rows[k];
-				// Если вытащили значение, сумма которого меньшее — передвигаем элемент с большей суммой дальше
-				if (current_sum < prev_sum) {
-					matrix[k + 1] = matrix[k];
-				}
-				else {
-					// Если вытащенный элемент больше — останавливаемся
-					break;
-				}
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (matrix[i][j] < min)
+					min = matrix[i][j];
+				else if (matrix[i][j] > max)
+					max = matrix[i][j];
+				else continue;
 			}
-			// В освободившееся место вставляем вытащенное значение
-			matrix[k + 1] = value;
+			min_elements.push_back(min);
+			max_elements.push_back(max);
 		}
+		min_result = *min_element(min_elements.begin(), min_elements.end());
+		max_result = *max_element(max_elements.begin(), max_elements.end());
+		results.push_back(1);
+		results.push_back(min_result);
+		results.push_back(max_result);
+		return results;
 	}
-};
+	else
+	{
+		results.push_back(0);
+		results.push_back(0);
+		results.push_back(0);
+		return results;
+	}
+}
 
+// реализация метода подсчета суммы над главной диагональю
+template<class T>
+auto CSqMatrix<T>::sum_above_diagonal()
+{
+	T sum = 0;
+	matrix.resize(size, std::vector<T>(size));
+	if (!matrix.empty())
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = 0; j < size; j++)
+			{
+				if (j > i)
+				{
+					sum += matrix[i][j];
+				}
+				else continue;
+			}
+		}
+		return std::make_pair(true, sum);
+	}
+	else {
+		return std::make_pair(false, sum);
+	}
+}
+
+// реализация метода для подсчета суммы строки
+template<class T>
+auto CSqMatrix<T>::row_amount(int row_number)
+{
+	T row_sum;
+	std::vector<T> sum_row;
+	if (!matrix.empty() and row_number <= size)
+	{
+		for (int j = 0; j < size; j++)
+			sum_row.push_back(matrix[row_number][j]);
+	}
+	row_sum = std::accumulate(sum_row.begin(), sum_row.end(), T(0));
+	return row_sum;
+}
+
+// реализация метода сортировки
+template<class T>
+void CSqMatrix<T>::sort_matrix()
+{
+	for (int i = 0; i < size; i++) {
+		auto value = matrix[i];
+		auto current_sum = row_amount(i);
+		int k = i - 1;
+		for (; k >= 0; k--) {
+			auto prev_sum = row_amount(k);
+			if (current_sum < prev_sum) {
+				matrix[k + 1] = matrix[k];
+			}
+			else {
+				break;
+			}
+		}
+		matrix[k + 1] = value;
+	}
+}
 #endif 
